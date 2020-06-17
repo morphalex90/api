cbb8f3bdf5d25c85526b8288e91cc97c1a7f5a87
morphalex90 <piero.nanni@gmail.com>
2020-06-17 21:41:19 +0100

    removed forcing of star table instead of stars, add vote to be filled

--- app/Star.php
+++ app/Star.php
@@ -3,10 +3,6 @@
 use Illuminate\Database\Eloquent\Model;
  
 class Star extends Model { 
-
-    protected $table = 'star';
-
-    // disable column updated_at
     const UPDATED_AT = null;
-    protected $fillable = ['created_at'];
+    protected $fillable = ['vote', 'created_at'];
 }
