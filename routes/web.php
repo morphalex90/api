7c309b37c87719581fb09a0aac69be0c70f61f90
morphalex90 <piero.nanni@gmail.com>
2020-06-17 02:16:54 +0100

    add custom routes for create stars and get the average

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,31 @@
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
 */
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// $router->get('/','StarController@averageStar'); ##### Return average value
+// $router->get('create','StarController@createStarr'); ##### Create new star
+
+########
+######## TOOLS
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) { // api/v1
+    $router->group(['prefix' => 'tools'], function () use ($router) { // api/v1/tools
+
+        $router->post('star','StarController@createStar'); ##### Create new star - POST api/v1/tools/star
+        $router->get('average_star','StarController@averageStar'); ##### Return average value - GET api/v1/tools/average_star
+    });
+});
+
+
+// $router->group(['prefix' => 'api/v1','namespace' => 'router\Http\Controllers'], function($router) {
+//   $router->post('car','CarController@createCar');
+//   $router->put('car/{id}','CarController@updateCar');
+    
+//   $router->delete('car/{id}','CarController@deleteCar');
+//   $router->get('car','CarController@index');
+// });
