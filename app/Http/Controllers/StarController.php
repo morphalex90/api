f9c620c136034d71be57d144b1c8eed82248922e
morphalex90 <piero.nanni@gmail.com>
2020-06-19 00:27:53 +0100

    add all the steps

--- app/Http/Controllers/StarController.php
+++ app/Http/Controllers/StarController.php
@@ -21,4 +21,130 @@ class StarController extends Controller {
         $averageStars = $stars->count();
         return response()->json(['count' => $averageStars, 'average' => $countStars]);
     }
+
+    public function stepLink(Request $request) {
+        $data = $request->all();
+
+        $response = 'Links bla bla bla';
+        $count = 25;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepImage(Request $request) {
+        $data = $request->all();
+
+        $response = 'Image bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepHeading(Request $request) {
+        $data = $request->all();
+
+        $response = 'Heading bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepMeta(Request $request) {
+        $data = $request->all();
+
+        $response = 'Meta bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepRobots(Request $request) {
+        $data = $request->all();
+
+        $response = 'Robots bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepSitemap(Request $request) {
+        $data = $request->all();
+
+        $response = 'Sitemap bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepOthers(Request $request) {
+        $data = $request->all();
+
+        $response = 'Others bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepStructuredData(Request $request) {
+        $data = $request->all();
+
+        $response = 'StructuredData bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
+
+    public function stepErrors(Request $request) {
+        $data = $request->all();
+
+        $response = 'Errors bla bla bla';
+        $count = 40;
+
+        $url = $data['url'];
+        $id = $data['id'];
+        $auth_username = $data['auth_username'];
+        $auth_password = $data['auth_password'];
+
+        return response()->json(['count' => $count, 'response' => $response]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,15 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) { // api/v1
 
         $router->post('star','StarController@createStar'); ##### Create new star - POST api/v1/tools/star
         $router->get('average_star','StarController@averageStar'); ##### Return average value - GET api/v1/tools/average_star
+
+        $router->post('step_link','StarController@stepLink'); ##### POST api/v1/tools/step_link
+        $router->post('step_image','StarController@stepImage'); ##### POST api/v1/tools/step_image
+        $router->post('step_heading','StarController@stepHeading'); ##### POST api/v1/tools/step_heading
+        $router->post('step_meta','StarController@stepMeta'); ##### POST api/v1/tools/step_meta
+        $router->post('step_robots','StarController@stepRobots'); ##### POST api/v1/tools/step_robots
+        $router->post('step_sitemap','StarController@stepSitemap'); ##### POST api/v1/tools/step_sitemap
+        $router->post('step_others','StarController@stepOthers'); ##### POST api/v1/tools/step_others
+        $router->post('step_structured_data','StarController@stepStructuredData'); ##### POST api/v1/tools/step_structured_data
+        $router->post('step_errors','StarController@stepErrors'); ##### POST api/v1/tools/step_errors
     });
 });
-
-
-// $router->group(['prefix' => 'api/v1','namespace' => 'router\Http\Controllers'], function($router) {
-//   $router->post('car','CarController@createCar');
-//   $router->put('car/{id}','CarController@updateCar');
-    
-//   $router->delete('car/{id}','CarController@deleteCar');
-//   $router->get('car','CarController@index');
-// });
