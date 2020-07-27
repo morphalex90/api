2013c31d29000524879c545e1edcd7ccbe1720f0
morphalex90 <piero.nanni@gmail.com>
2020-07-27 02:25:42 +0100

    player leave session url

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) { // api/v1
         $router->get('session/{id}/players','YahtzeeSessionController@players'); ##### Get all players of a session - GET api/v1/yahtzee/session/players
 
         $router->post('session/player','YahtzeePlayerController@store'); ##### Create new player - POST api/v1/yahtzee/session/player
+        $router->post('session/{session_id}/player/{player_id}/leave','YahtzeePlayerController@player_leave'); ##### Leave player from a session - GET api/v1/yahtzee/session/players
 
     });
 });
