0bc0669a0efedb6e53bf5e15ba96dbdce33d6125
morphalex90 <piero.nanni@gmail.com>
2022-06-05 15:31:19 +0100

    move Tools to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ToolsScanController;
+use App\Http\Controllers\ToolsStarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+########
+######## TOOLS
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'tools'], function () {
+
+        Route::post('star', [ToolsStarController::class, 'store']); ##### Create new star - POST api/v1/tools/star
+        Route::get('average_star', [ToolsStarController::class, 'averageStar']); ##### Return average value - GET api/v1/tools/average_star
+
+        Route::post('scans', [ToolsScanController::class, 'store']);
+        Route::get('scans/{scan_uuid}', [ToolsScanController::class, 'show']);
+        Route::get('scans/{scan_uuid}/step_links', [ToolsScanController::class, 'stepLinks']);
+        Route::get('scans/{scan_uuid}/step_images', [ToolsScanController::class, 'stepImages']);
+        Route::get('scans/{scan_uuid}/step_headings', [ToolsScanController::class, 'stepHeadings']);
+        Route::get('scans/{scan_uuid}/step_meta', [ToolsScanController::class, 'stepMeta']);
+        Route::get('scans/{scan_uuid}/step_robots', [ToolsScanController::class, 'stepRobots']);
+        Route::get('scans/{scan_uuid}/step_sitemap', [ToolsScanController::class, 'stepSitemap']);
+        Route::get('scans/{scan_uuid}/step_others', [ToolsScanController::class, 'stepOthers']);
+        Route::get('scans/{scan_uuid}/step_structured_data', [ToolsScanController::class, 'stepStructuredData']);
+    });
 });
